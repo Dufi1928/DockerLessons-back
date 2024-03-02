ae41801795078e15bc072fe9f2cd44016a229d22
ivanh <i.hrybe@gmail.com>
2024-03-02 16:10:29 +0100

    feat: git actions added

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Attendre que la base de données soit prête
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+# Appliquer les migrations Django
+python manage.py migrate --noinput
+
+# Démarrer Gunicorn avec l'application Django
+gunicorn --workers=3 --bind=0.0.0.0:8000 myproject.wsgi:application
